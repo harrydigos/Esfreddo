c97d87b1c37b92f10e99ef298c90828e83b60243
BabisDigos <ntigos2001@gmail.com>
2022-11-07 19:50:50 +0200

    Supabase connected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../layouts/layout";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionContextProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import { useSession } from "@supabase/auth-helpers-react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <></>;
+  const session = useSession();
+
+  return (
+    <div className="flex justify-center items-center">
+      {!session ? (
+        <p>Not Logged</p>
+      ) : (
+        <p>Logged In</p>
+      )}
+    </div>
+  );
 };
 
 export default Home;
