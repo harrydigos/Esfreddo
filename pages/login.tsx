43bcc996fe65899d5389b578628a5d4a7c176050
BabisDigos <ntigos2001@gmail.com>
2022-11-07 15:55:02 +0200

    Form impovements

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,17 @@
 import type { NextPage } from "next";
+import React from "react";
 
 const Login: NextPage = () => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const email = document.querySelector("#email") as HTMLInputElement;
+    const password = document.querySelector("#password") as HTMLInputElement;
+    const remember = document.querySelector("#remember") as HTMLInputElement;
+
+    console.log("Data: ", email.value, password.value, remember.checked);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="flex flex-col items-start bg-white gap-6">
@@ -8,28 +19,42 @@ const Login: NextPage = () => {
           <h1 className="text-3xl font-semibold">Login</h1>
           <p>Welcome back. Please enter your details</p>
         </div>
-        <form className="flex flex-col w-full gap-6">
+        <form
+          onSubmit={handleSubmit}
+          action="/"
+          method="post"
+          className="flex flex-col w-full gap-6"
+        >
           <div className="flex flex-col items-start gap-2">
-            <label className="w-full text-left font-medium">Email</label>
+            <label htmlFor="email" className="w-full text-left font-medium">
+              Email
+            </label>
             <input
               className="w-full border-2 p-2 rounded"
               type="email"
+              name="email"
+              id="email"
               placeholder="Enter your email"
             />
           </div>
           <div className="flex flex-col items-start gap-2">
-            <label className="w-full text-left font-medium">Password</label>
+            <label htmlFor="password" className="w-full text-left font-medium">
+              Password
+            </label>
             <input
               className="w-full border-2 p-2 rounded"
               type="password"
+              name="password"
+              id="password"
               placeholder="Enter your password"
             />
           </div>
-          <div className="flex justify-between">
-            <div className="flex items-center gap-2">
-              <input type="checkbox" className="w-4 h-4 border-2 border-coffee-dark accent-coffee-dark rounded hover:cursor-pointer" />
-              <label>Remember me</label>
-            </div>
+          <div className="flex justify-between group">
+            <label htmlFor="remember" className="checkbox-container">
+              <input type="checkbox" name="remember" id="remember" />
+              <div className="checkmark" />
+              <div className="text" data-text="Remember me" />
+            </label>
             <a href="#" className="font-semibold">
               Forgot password
             </a>
@@ -43,7 +68,6 @@ const Login: NextPage = () => {
           <div className="text-center">
             Don't have an account?
             <a href="/" className="font-semibold">
-              {" "}
               Sign up
             </a>
           </div>
