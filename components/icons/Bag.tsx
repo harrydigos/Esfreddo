a90537bcc795d95932caf92255caf2f9a68e6618
BabisDigos <ntigos2001@gmail.com>
2022-11-11 18:28:36 +0200

    Navbar profile

---
+++ components/icons/Bag.tsx
@@ -0,0 +1,39 @@
+import { SVGProps } from "react";
+
+const Bag: React.FC<SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M7.5 7.67001V6.70001C7.5 4.45001 9.31 2.24001 11.56 2.03001C14.24 1.77001 16.5 3.88001 16.5 6.51001V7.89001"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.99999 22H15C19.02 22 19.74 20.39 19.95 18.43L20.7 12.43C20.97 9.99 20.27 8 16 8H7.99999C3.72999 8 3.02999 9.99 3.29999 12.43L4.04999 18.43C4.25999 20.39 4.97999 22 8.99999 22Z"
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15.4955 12H15.5045"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.49451 12H8.50349"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default Bag;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,54 @@
+import { useSession, useUser } from "@supabase/auth-helpers-react";
 import Link from "next/link";
 import { FC } from "react";
+import Bag from "./icons/Bag";
 
 type NavbarProp = JSX.IntrinsicElements["nav"];
 
 export const Navbar: FC<NavbarProp> = () => {
+  const session = useSession();
+  const user = useUser();
+
+  // const [firstName] = user?.user_metadata.full_name.split(" ") || [];
+
   return (
     <nav className="absolute top-0 z-30 w-full border-b bg-white">
-      <div className="container mx-auto flex items-center justify-between py-3 font-semibold text-lg text-coffee-dark">
-        <div>
+      <div className="container h-14 mx-auto flex items-center justify-between font-semibold text-lg text-dark">
+        <div className="flex items-center justify-center gap-6">
           <Link href="/" className="font-extrabold text-2xl">
             ESFREDDO
           </Link>
+          <div className="w-[2px] h-6 bg-coffee-cream rounded-full"></div>
+          <div className="flex items-center justify-center gap-6">
+            <Link href="/">Home</Link>
+            <a href="#">Products</a>
+            <a href="#">Stores</a>
+            <a href="#">Company</a>
+          </div>
         </div>
-        <div className="absolute left-1/2 -translate-x-1/2 flex justify-center items-center gap-6">
-          <Link href="/">Home</Link>
-          <a href="#">Products</a>
-          <a href="#">Stores</a>
-          <a href="#">Company</a>
-        </div>
-        <div className="flex justify-center items-center gap-6">
-          <Link href="/login">Login</Link>
-          <button className="inline-block rounded-md bg-coffee-dark text-coffee-cream-light px-4 py-1">
-            <Link href="/signUp">Sign up</Link>
-          </button>
-        </div>
+
+        {!session ? (
+          <div className="flex justify-center items-center gap-6">
+            <Link href="/login">Login</Link>
+            <button className="inline-block rounded-md bg-coffee-dark text-coffee-cream-light px-4 py-1">
+              <Link href="/signUp">Sign up</Link>
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="flex items-center gap-6">
+              <div className="w-8 h-8 flex justify-center items-center bg-coffee-cream rounded-full">
+                <Bag stroke="#483434" width={24} height={24} />
+              </div>
+              <div className="flex items-center gap-3">
+                <div className="whitespace-nowrap max-w-[140px] overflow-hidden text-ellipsis text-coffee-light">
+                  {user?.user_metadata.full_name.split(" ")[0]}
+                </div>
+                <div className="w-10 h-10 rounded-full bg-[#C4CDEE]"></div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </nav>
   );
