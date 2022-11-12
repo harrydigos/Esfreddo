9e86c7fbc9306e5568ebe7603b2a8323d7995e5b
BabisDigos <ntigos2001@gmail.com>
2022-11-12 23:59:42 +0200

    New navbar design

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,30 +1,29 @@
 import { useSession, useUser } from "@supabase/auth-helpers-react";
 import Link from "next/link";
 import { FC } from "react";
-import Bag from "./icons/Bag";
+import Bag from "./icons/BagIcon";
 
-type NavbarProp = JSX.IntrinsicElements["nav"];
+type NavbarProps = JSX.IntrinsicElements["nav"];
 
-export const Navbar: FC<NavbarProp> = () => {
+export const Navbar: FC<NavbarProps> = () => {
   const session = useSession();
   const user = useUser();
 
-  // const [firstName] = user?.user_metadata.full_name.split(" ") || [];
-
   return (
-    <nav className="absolute top-0 z-30 w-full border-b bg-white">
-      <div className="container h-14 mx-auto flex items-center justify-between font-semibold text-lg text-dark">
+    <nav className="absolute bg-transparent top-2 z-30 w-full">
+      <div
+        className="container h-14 px-4 mx-auto flex items-center justify-between font-semibold text-lg text-dark
+      drop-shadow-[0px_0px_16px_rgba(41,41,41,0.2)] backdrop-blur-[4px] bg-white/50 border border-white/20 rounded-2xl"
+      >
         <div className="flex items-center justify-center gap-6">
           <Link href="/" className="font-extrabold text-2xl">
             ESFREDDO
           </Link>
           <div className="w-[2px] h-6 bg-coffee-cream rounded-full"></div>
-          <div className="flex items-center justify-center gap-6">
-            <Link href="/">Home</Link>
-            <a href="#">Products</a>
-            <a href="#">Stores</a>
-            <a href="#">Company</a>
-          </div>
+          <Link href="/">Home</Link>
+          <a href="#">Products</a>
+          <Link href="/stores">Stores</Link>
+          <a href="#">Company</a>
         </div>
 
         {!session ? (
@@ -41,7 +40,7 @@ export const Navbar: FC<NavbarProp> = () => {
                 <Bag stroke="#483434" width={24} height={24} />
               </div>
               <div className="flex items-center gap-3">
-                <div className="whitespace-nowrap max-w-[140px] overflow-hidden text-ellipsis text-coffee-light">
+                <div className="whitespace-nowrap max-w-[140px] overflow-hidden text-ellipsis text-dark">
                   {user?.user_metadata.full_name.split(" ")[0]}
                 </div>
                 <div className="w-10 h-10 rounded-full bg-[#C4CDEE]"></div>
