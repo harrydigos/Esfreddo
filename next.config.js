28216615d2751cec30df0d65669f20b9eee0143f
BabisDigos <ntigos2001@gmail.com>
2022-11-13 19:55:07 +0200

    Added Mapbox gl

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN:
+      process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN,
+  },
+};
--- pages/stores.tsx
+++ pages/stores.tsx
@@ -1,21 +1,32 @@
 import { NextPage } from "next";
-import Image from "next/image";
-import map from "@public/map.jpg";
 import StoresSidebar from "@components/StoresSidebar";
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
 
 const Stores: NextPage = () => {
+  const mapContainer = useRef<any>(null);
+
+  mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN || "";
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [-74.006, 40.7128],
+      zoom: 12,
+    });
+    return () => map.remove();
+  }, []);
+
   return (
     <>
-    <div className="container mx-auto">
-      <StoresSidebar />
-
-      
-    </div>
-    <Image
-        src={map}
-        alt="Map"
-        className="w-screen h-screen object-cover"
-      ></Image>
+      <div className="container mx-auto">
+        <StoresSidebar />
+      </div>
+      <div
+        className="w-screen h-screen overflow-hidden"
+        ref={mapContainer}
+      ></div>
     </>
   );
 };
