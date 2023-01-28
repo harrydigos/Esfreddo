21c33e5aba5ba4ab27c0d0b52912b59bab8bd125
panteliselef <panteliselef@outlook.com>
2023-01-28 16:42:48 +0100

    Use a callback to store cart in localStorage + Removing unnecessary state for the Navber animation

--- components/navbar/NavbarProvider.tsx
+++ components/navbar/NavbarProvider.tsx
@@ -1,9 +1,10 @@
-import { createContext, FC, PropsWithChildren, useContext, useEffect, useState } from "react";
+import { createContext, FC, PropsWithChildren, useContext, useEffect, useMemo, useState } from "react";
 
 const NavbarContext = createContext({ visible: true });
 
+let prevYOffset = 0;
+
 const NavbarProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [, setYOffset] = useState(0);
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
@@ -12,10 +13,8 @@ const NavbarProvider: FC<PropsWithChildren> = ({ children }) => {
   }, []);
 
   const handleScroll = () => {
-    setYOffset((oldValue) => {
-      setVisible(oldValue > window.pageYOffset || window.pageYOffset < 64);
-      return window.pageYOffset;
-    });
+    setVisible(prevYOffset > window.pageYOffset || window.pageYOffset < 64);
+    prevYOffset = window.pageYOffset;
   };
 
   return <NavbarContext.Provider value={{ visible }}>{children}</NavbarContext.Provider>;
